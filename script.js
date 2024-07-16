0358d1e2eae2a600f76611ba8578667f3658f19f
Dat P Nguyen <darkexpert3@gmail.com>
2024-07-16 09:11:28 +0700

    add feature to highlight appropriate button when script is loaded

--- script.js
+++ script.js
@@ -12,6 +12,16 @@ nodesNavButtons.forEach((node) => {
 
 // console.log(arrayNavLinks)
 
+//change the background color of matching nav button of current iframe when page is loaded. This part is not neccessary if you already add "current-iframe" class to appropriate button manually beforehand. Because when iframe is changed, the entire page is NOT reloaded, so you're basically going to the same iframe link you set in the HTML document when visiting the same SITE URL. You can always expect the same iframe url to load when you go to the site, so you can just manually add the class to that default iframe.
+
+for (let i = 0; i <= arrayNavLinks.length - 1; i++) {
+  const currentIframeSRC = iframe.getAttribute('src')
+  if (currentIframeSRC === arrayNavLinks[i]) {
+    nodesNavButtons[i].classList.add('current-iframe')
+  }
+}
+
+// When button is clicked, unhighlight all buttons, then highlight the appropriate button:
 nodesNavButtons.forEach((node) => {
   node.addEventListener('click', (event) => {
     const href = event.target.getAttribute('data-href')
@@ -26,7 +36,7 @@ nodesNavButtons.forEach((node) => {
   })
 })
 
-// Becareful when adding a "click" event listener to an element and using event.target. The event.target in this scenario IS NOT the element you addEventListener to, but event.target will be the element that you click.
+// Note to self: Becareful when adding a "click" event listener to an element and using event.target. The event.target in this scenario IS NOT the element you addEventListener to, but event.target will be the element that you click.
 
 // So in the case of having a click listener in a parent button with nested child elements, because the button you try to click on has child element inside it. You add the neccessary attribute in the parent button element instead, so if you accidentally click on the child element inside it (example: text element), it will return nothing because you didn't add the attribute you're trying to get through JS to the child element in HTML document.
 
